450fc29d5c3c01805465b49a2810c60037069105
LAPTOP-IUSC7P5E\Piotr <pgardocki@interia.pl>
2019-06-15 20:07:11 +0200

    Add test running code in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include "MainWindow.hpp"
 #include <QApplication>
 
+#define CATCH_CONFIG_RUNNER
+#include "vendor/catch.hpp"
+
 int main(int argc, char *argv[])
 {
+    Catch::Session().run(argc, argv);
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
