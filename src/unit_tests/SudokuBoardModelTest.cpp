fd6a4dd2169d4ef35adbabf6270a093a7c3d507f
LAPTOP-IUSC7P5E\Piotr <pgardocki@interia.pl>
2019-06-18 21:44:39 +0200

    Add test cases for SudokuBoardModel

--- src/unit_tests/SudokuBoardModelTest.cpp
+++ src/unit_tests/SudokuBoardModelTest.cpp
@@ -38,3 +38,22 @@ TEST_CASE("SudokuBoardModel - setting value", "[sudoku][model][qt]")
         CHECK(model.data(index, Qt::DisplayRole).toString() == "3");
     }
 }
+
+TEST_CASE("SudokuBoardModel - fields' flags", "[sudoku][model][qt]")
+{
+    SudokuBoardModel model;
+    QModelIndex index = model.index(2, 4);
+
+    Qt::ItemFlags correctFlags = Qt::ItemIsEnabled | Qt::ItemIsEditable | Qt::ItemNeverHasChildren;;
+
+    CHECK(model.flags(index) == correctFlags);
+    CHECK(model.flags(QModelIndex()) == Qt::NoItemFlags);
+}
+
+TEST_CASE("SudokuBoardModel - none header data", "[sudoku][model][qt]")
+{
+    SudokuBoardModel model;
+
+    CHECK(model.headerData(0, Qt::Horizontal, Qt::DisplayRole) == QVariant());
+    CHECK(model.headerData(0, Qt::Vertical, Qt::DisplayRole) == QVariant());
+}
