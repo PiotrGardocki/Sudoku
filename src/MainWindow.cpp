a85af9021b54f27fe4f057d5ba7323a02ad84260
Piotr Gardocki <pgardocki@interia.pl>
2019-06-29 19:06:31 +0200

    Add initial code to MainWindow

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -1,8 +1,17 @@
 #include "MainWindow.hpp"
+#include "SudokuBoardView.hpp"
 
-MainWindow::MainWindow(QWidget *parent)
+#include <QVBoxLayout>
+
+MainWindow::MainWindow(QWidget * parent)
     : QMainWindow(parent)
 {
+    QWidget * widget = new QWidget;
+    setCentralWidget(widget);
 
+    QVBoxLayout * lay = new QVBoxLayout;
+    widget->setLayout(lay);
 
+    SudokuBoardView * view = new SudokuBoardView;
+    lay->addWidget(view);
 }
